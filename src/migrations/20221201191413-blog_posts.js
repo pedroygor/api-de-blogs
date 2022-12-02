8bcff60ce35224da9394d30927b8f36becbb9902
pedroygor <pedroygor2010@hotmail.com>
2022-12-01 23:08:10 -0300

    feat: migrations and relationships created

---
+++ src/migrations/20221201191413-blog_posts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id : {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false, 
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: null,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users', 
+          key: 'id'
+        },
+      }, 
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
