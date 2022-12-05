a2e3ec8035788813d68dd293c72574d41fe7c74f
pedroygor <pedroygor2010@hotmail.com>
2022-12-05 20:34:47 -0300

    feat: create model BlogPost

--- src/models/User.js
+++ src/models/User.js
@@ -12,5 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, 
+      { foreignKey: 'userId', as: 'posts' });
+  };
+
   return User;
 }
